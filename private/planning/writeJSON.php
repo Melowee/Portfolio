3a75eb110364432827b97c0937b12759b1be5b60
Clement Dauchez <clement_dauchez@ens.univ-artois.fr>
2019-04-09 17:55:31 +0200

    enlevage du planning

--- private/planning/writeJSON.php
+++
@@ -1,7 +0,0 @@
-<?php
-    header('Location: https://dauchez-clement-portfolio.000webhostapp.com/private/planning/planning.html');
-    
-    $jsonFile = fopen("animes.json", "w");
-    fwrite($jsonFile, $_GET['json']);
-    fclose($jsonFile);
-?>
